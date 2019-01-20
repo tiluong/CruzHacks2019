ab544c6beeda436712d09eefaa33508fbe7e3c36
Sherri Li <shli17@users.noreply.github.com>
2019-01-19 22:27:24 -0800

    update buttons and activities
    link up the signin and signup buttons

--- app/src/main/java/tiff/cruzhacks2019/SignIn.java
+++ app/src/main/java/tiff/cruzhacks2019/SignIn.java
@@ -1,32 +1,21 @@
 package tiff.cruzhacks2019;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.List;
-import java.util.Locale;
+import android.content.Intent;
 
 
 public class SignIn extends AppCompatActivity {
     // Signing in brings you back to your instant messaging page where you can message
     // your mentors, and/or your mentees.
-    Button inboxBtn;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        //setContentView(R.layout.activity_signin);
+        setContentView(R.layout.signin); // display the signin XML we created
+        android.support.v7.app.ActionBar menu = getSupportActionBar();
+
 
     }
 
